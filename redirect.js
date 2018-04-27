678f1642e5e436f45c800cc5428237b391cca75d
Harshit <harshitgambhir88@gmail.com>
2018-04-27 16:25:18 +0530

    added cache control header in redirect

--- redirect.js
+++ redirect.js
@@ -1,20 +1,19 @@
 const isLength = require('validator/lib/isLength');
 const db = require('./db/db');
 const redirect = async (req, res, next) => {
-    console.log('a')
+    res.setHeader('Cache-Control', 'no-cache');
     const short_url_id = req.params.short_url_id;
     if(!isLength(short_url_id, {min: 8, max:8})){
         return res.send("<div>404</div>");
     }
     try{
         const result = await db.get().collection('urls').findOneAndUpdate({_id: short_url_id}, {$inc: {count: 1}}, {returnOriginal: true});
-        
         if(!result.value){
             return res.send("<div>404</div>");
         }
-        res.redirect(301,result.value.long_url);
+        res.redirect(result.value.long_url);
     } catch(error){
-        console.log(error)
+
     }
 }
 
